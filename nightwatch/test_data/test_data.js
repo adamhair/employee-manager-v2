72edc9a08c4a248c855ba0d0a6d0d936b73d0a23
Jake Shober <jake@Jakes-MacBook-Air.local>
2017-12-22 12:16:03 -0700

    added good/bad Data

--- nightwatch/test_data/test_data.js
+++ nightwatch/test_data/test_data.js
@@ -1,5 +1,18 @@
 const selectors = require('../test_data/css_selectors')
 
 module.exports = {
-   
+   //goodData is pulled from case scenarios 1-4 found in https://docs.google.com/spreadsheets/d/1wWavN8CVRF67_taRfpiuzYyp10XHs_WJX9s9BAP6xh0/edit#gid=0
+   goodData: {
+       name: 'Superman',
+       phone: '8016783456',
+       email: 'hello@gmail.com',
+       title: 'COO'
+   },
+   //badData is derived from the README.md file, partial boundary testing.
+   badData: {
+       name: 'asdfjklapoiurewq123456789nbmvcnbadfebad',
+       phone: '123456789',
+       email: 'asdfjklapoiurewq123456789nbmvcnbadfebad',
+       title: 'asdfjklapoiurewq123456789nbmvcnbadfebad'
+   }
 }
